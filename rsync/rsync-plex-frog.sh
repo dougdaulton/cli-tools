eef0bcdd17c8ea15ba7f3704d942ed6cd50243d2
dougdaulton <doug.daulton@gmail.com>
2015-07-10 10:03:54 -0700

    polish script for final usage.
    Remove username/hostname and made them variables to pass in.

--- rsync/rsync-plex-frog.sh
+++ rsync/rsync-plex-frog.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# rsync PLEX: KODIAK to FROG (TEST STRINGS)
+# rsync-plex.sh
 # ------------------------------------------------------------------
-# rsync -avzn -e "ssh -i /home/bayaz/cron/frog-rsync-key" bayaz@KODIAK:/VOLUMES/KODIAK4TB1/PLEX_MOVIES/ /VOLUMES/FROG4TB/PLEX/MOVIES/
-# rsync -avzn -e "ssh -i /home/bayaz/cron/frog-rsync-key" bayaz@KODIAK:/VOLUMES/KODIAK4TB1/PLEX_TV/ /VOLUMES/FROG4TB/PLEX/TV_SHOWS/
-# rsync -avzn -e "ssh -i /home/bayaz/cron/frog-rsync-key" bayaz@KODIAK:/VOLUMES/KODIAK2TB/PLEX_MUSIC/ /VOLUMES/FROG4TB/PLEX/MUSIC/
-# rsync -avzn -e "ssh -i /home/bayaz/cron/frog-rsync-key" bayaz@KODIAK:/VOLUMES/KODIAK2TB/PLEX_AUDIOBOOKS/ /VOLUMES/FROG4TB/PLEX/AUDIOBOOKS/
+# 
+# This script is used to sync/backup media directories from a central 
+# PLEX server. Run via crontab once a day, in the early AM hours.
 
+
+# RUNNING THE SCRIPT
+# ------------------------------------------------------------------
+
+# From the BASH shell
+
+# bash rsync-plex.sh sync-key RUSER RHOST RVOLUME
+
+# From Cron
+
+# 0 0 * * * rsync-plex.sh rsync-key RUSER RHOST RVOLUME
 
 
 # SET BASE VARIABLES
@@ -14,8 +24,9 @@
 RSYNC=/usr/bin/rsync 
 SSH=/usr/bin/ssh 
 KEY=/home/bayaz/cron/frog-rsync-key
-RUSER=bayaz 
-RHOST=kodiak
+RUSER=rsync-key[1]
+RHOST=rsync-key[2]
+RPATH=/VOLUMES/rsync-key[3]/PLEX
 
 REMOTEPATH=(
 	[0]=/VOLUMES/KODIAK4TB1/PLEX_MOVIES
@@ -25,10 +36,10 @@ REMOTEPATH=(
 )
 
 LOCALPATH=(
-	[0]=/VOLUMES/FROG4TB/PLEX/MOVIES
-	[1]=/VOLUMES/FROG4TB/PLEX/TV_SHOWS
-	[2]=/VOLUMES/FROG4TB/PLEX/MUSIC
-	[3]=/VOLUMES/FROG4TB/PLEX/AUDIO
+	[0]=$RPATH/MOVIES
+	[1]=$RPATH/TV_SHOWS
+	[2]=$RPATH/MUSIC
+	[3]=$RPATH/AUDIO
 )
 
 # LOOP THROUGH DIRS TO EXECUTE RSYNC
