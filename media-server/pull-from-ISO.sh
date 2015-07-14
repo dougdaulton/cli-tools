875f70d4bd627ef2bb642b7c19324831406f1957
dougdaulton <doug.daulton@gmail.com>
2015-07-13 22:16:42 -0700

    add pull-from-ISO script stub

---
+++ media-server/pull-from-ISO.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# -----------------------------------------------------
+# Pull FIles From ISO
+# -----------------------------------------------------
+
+# From the BASH shell
+
+# bash pull-from-ISO.sh ISO-key SOURCEDIR WRITEDIR
+
+
+# Establish Variables
+# -----------------------------------------------------
+SOURCEDIR=$1
+WRITEDIR=$2
+
+# Read the Directory
+# -----------------------------------------------------
+
+arr=(~/myDir/*)
+
+# iterate through array using a counter
+for ((i=0; i<${#arr[@]}; i++)); do
+    #do something to each element of array
+    echo "${arr[$i]}"
+done
+
+# Remove Selected String
+# -----------------------------------------------------
+
+# Convert Case to CamelCase
+# -----------------------------------------------------
+
+# Cleanup Loose Ends
+# -----------------------------------------------------
