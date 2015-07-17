56c9112ec1aebd226a9e7550d4b2d24c44302bd6
Doug Daulton <doug.daulton@gmail.com>
2015-07-17 14:33:28 -0700

    1. Clean structure to complete one pass at a time 2. Stub out dir removal

--- tests.py
+++ tests.py
@@ -30,23 +30,23 @@ else:
 # Set targetdirs & file extension lists
 # --------------------------------------------------------
 
-dir_isos = "/go/TR/02_TARGET/00_ISOS"                                # Watch Folder:  ISOs
+target_isos = "/go/TR/02_TARGET/00_ISOS"                                # Watch Folder:  ISOs
 
-dir_tarballs = "/go/TR/02_TARGET/01_TARBALLS"                        # Watch Folder: Tarballs
+target_tarballs = "/go/TR/02_TARGET/01_TARBALLS"                        # Watch Folder: Tarballs
 
-dir_reencodes = "/go/TR/02_TARGET/02_REENCODES"                      # Watch Folder: Reencodes
+target_reencodes = "/go/TR/02_TARGET/02_REENCODES"                      # Watch Folder: Reencodes
 
-#dir_isos = "/go/PROCESS/00_ISOS"                                # Watch Folder:  ISOs
+#target_isos = "/go/PROCESS/00_ISOS"                                # Watch Folder:  ISOs
 
-#dir_tarballs = "/go/PROCESS/01_TARBALLS"                        # Watch Folder: Tarballs
+#target_tarballs = "/go/PROCESS/01_TARBALLS"                        # Watch Folder: Tarballs
 
-#dir_reencodes = "/go/PROCESS/02_REENCODE"                        # Watch Folder: Reencodes
+#target_reencodes = "/go/PROCESS/02_REENCODE"                        # Watch Folder: Reencodes
 
-files_isos = [".iso",".ISO"]                                    # File Extensions (ISOs)
+exts_isos = [".iso",".ISO"]                                    # File Extensions (ISOs)
 
-files_tarballs= [".tar",".TAR",".tgz",".TGZ",".zip",".ZIP"]     # File Extensions (Tarballs)
+exts_tarballs= [".tar",".TAR",".tgz",".TGZ",".zip",".ZIP"]     # File Extensions (Tarballs)
 
-files_reencodes = [".avi",".AVI",".flv",".FLV",".mpg",".MPG"]   # File Extensions (Reencodes)
+exts_reencodes = [".avi",".AVI",".flv",".FLV",".mpg",".MPG"]   # File Extensions (Reencodes)
 
 
 # FIND & MOVE FILES
@@ -56,26 +56,45 @@ for dirpath, dirs, files in os.walk(sourcedir):                 # Parse The Dire
     for f in files:
         fname, fext = os.path.splitext(f)                       # Split files into basename & ext
         
-        if any(fext in x for x in files_isos):                  # FIND & MOVE ISOS
-            targetpath = dir_isos                               # Set target directory
+        if any(fext in x for x in exts_isos):                  # FIND & MOVE ISOS
+            targetpath = target_isos                               # Set target directory
            
             print ("mv "+dirpath+"/"+f+" "+targetpath+"/"+f)    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
 
-        if any(fext in x for x in files_tarballs):              # FIND & MOVE TARBALLS
-            targetpath = dir_tarballs                           # Set target directory
+            iso_dirs = iso_dirs.extend(dirpath) 
+            return iso_dirs
+
+        elif any(fext in x for x in exts_tarballs):              # FIND & MOVE TARBALLS
+            targetpath = target_tarballs                           # Set target directory
             
             print ("mv "+dirpath+"/"+f+" "+targetpath+"/"+f)    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
 
-        if any(fext in x for x in files_reencodes):             # FIND & MOVE Reencodes
-            targetpath = dir_reencodes                          # Set target directory
+            tarball_dirs = tarball_dirs.extend(dirpath) 
+            return tarball_dirs
+
+        elif any(fext in x for x in exts_reencodes):             # FIND & MOVE Reencodes
+            targetpath = target_reencodes                          # Set target directory
 
 	     	
             dir2move = os.path.basename(dirpath)
             print ("mv "+dirpath+" --> "+targetpath+"/")    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
 
+        else:
+            print "# ------------------------------------"
+            print "# NOTHING TO MOVE"
+            print "# ------------------------------------"
+
+
+# REMOVE EMPTY DIRECTORIES
+# --------------------------------------------------------
+            iso_dirs = set(iso_dirs)
+            print ("++++++++++\n"+iso_dirs+"++++++++++\n\n") 
+
+            tarball_dirs = set(tarball_dirs)
+            print ("++++++++++\n"+tarball_dirs+"++++++++++\n") 
 # --------------------------------------------------------
 # EOF
 # --------------------------------------------------------	
