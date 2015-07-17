27c22d7c3d61b11cdea286f678d62fbfd2506527
Doug Daulton <doug.daulton@gmail.com>
2015-07-17 13:00:09 -0700

    Fixed bar names in test.py

--- tests.py
+++ tests.py
@@ -34,19 +34,19 @@ dir_isos = "/go/TR/02_TARGET/00_ISOS"                                # Watch Fol
 
 dir_tarballs = "/go/TR/02_TARGET/01_TARBALLS"                        # Watch Folder: Tarballs
 
-dir_reencode = "/go/TR/02_TARGET/02_REENCODES"                        # Watch Folder: Reencodes
+dir_reencodes = "/go/TR/02_TARGET/02_REENCODES"                      # Watch Folder: Reencodes
 
 #dir_isos = "/go/PROCESS/00_ISOS"                                # Watch Folder:  ISOs
 
 #dir_tarballs = "/go/PROCESS/01_TARBALLS"                        # Watch Folder: Tarballs
 
-#dir_reencode = "/go/PROCESS/02_REENCODE"                        # Watch Folder: Reencodes
+#dir_reencodes = "/go/PROCESS/02_REENCODE"                        # Watch Folder: Reencodes
 
 files_isos = [".iso",".ISO"]                                    # File Extensions (ISOs)
 
 files_tarballs= [".tar",".TAR",".tgz",".TGZ",".zip",".ZIP"]     # File Extensions (Tarballs)
 
-files_reencode = [".avi",".AVI",".flv",".FLV",".mpg",".MPG"]    # File Extensions (ReEncodes)
+files_reencodes = [".avi",".AVI",".flv",".FLV",".mpg",".MPG"]   # File Extensions (Reencodes)
 
 
 # FIND & MOVE FILES
@@ -56,20 +56,20 @@ for dirpath, dirs, files in os.walk(sourcedir):                 # Parse The Dire
     for f in files:
         fname, fext = os.path.splitext(f)                       # Split files into basename & ext
         
-        if any(fext in x for x in files_iso):                   # FIND & MOVE ISOS
-            targetdir = dir_isos                                # Set target directory
-            
+        if any(fext in x for x in files_isos):                  # FIND & MOVE ISOS
+            targetpath = dir_isos                               # Set target directory
+           
             print ("mv "+dirpath+"/"+f+" "+targetpath+"/"+f)    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
 
         if any(fext in x for x in files_tarballs):              # FIND & MOVE TARBALLS
-            targetdir = dir_tarballs                            # Set target directory
+            targetpath = dir_tarballs                           # Set target directory
             
             print ("mv "+dirpath+"/"+f+" "+targetpath+"/"+f)    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
 
         if any(fext in x for x in files_reencodes):             # FIND & MOVE Reencodes
-            targetdir = dir_reecode                             # Set target directory
+            targetpath = dir_reencodes                          # Set target directory
 
             print ("mv "+dirpath+"/"+f+" "+targetpath+"/"+f)    # Move File (Display)
 #           shutil.move(dirpath+"/"+f, targetpath+"/"+f)       # Move File (Execute)
